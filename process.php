db92aec63e0d840c3a5f64373532939b31fa2582
Martin Stenflo <stenflo@gmail.com>
2020-05-03 09:25:30 -0700

    contact form php script

---
+++ process.php
@@ -0,0 +1,14 @@
+<?php
+  if (isset($_POST['email']))  {
+  
+    //Email information
+    $admin_email = "stenflo@gmail.com";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    //send email
+    mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
+    
+    header('Location: http://edgeledger.net/success.html');
+  }
